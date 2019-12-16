845d72087ddbdb43afff93c0531be3643346469f
Leina33 <dominickrutto@gmail.com>
2019-12-16 08:46:11 +0300

    change the model location name to Location to avoid programming errors

--- personal/models.py
+++ personal/models.py
@@ -5,13 +5,18 @@ class Image(models.Model):
     name = models.CharField(max_length =60)
     description = models.TextField()
     
+    
     def __str__(self):
         return self.name
     
 
-class location(models.Model):
+class Location(models.Model):
     country = models.CharField(max_length = 60)
     
     def __str__(self):
         return self.location
+    
+    
+
+
     
