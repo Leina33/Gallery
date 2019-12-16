4be0fc3c3e42da6700e3094624df555ea7921139
Leina33 <dominickrutto@gmail.com>
2019-12-16 08:33:22 +0300

    this is a class location where the pictures location will be shown

--- personal/models.py
+++ personal/models.py
@@ -5,5 +5,13 @@ class Image(models.Model):
     name = models.CharField(max_length =60)
     description = models.TextField()
     
+    def __str__(self):
+        return self.name
     
+
+class location(models.Model):
+    country = models.CharField(max_length = 60)
+    
+    def __str__(self):
+        return self.location
     
