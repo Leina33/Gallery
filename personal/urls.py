5326963d0b7fb1ef165918be09afc40f1ae72a19
Leina33 <dominickrutto@gmail.com>
2019-12-16 10:00:38 +0300

    remove the welcome function and update the url route

--- personal/urls.py
+++ personal/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^today/$', views.personal_of_day, name = 'personalToday'),
+    # url('^$',views.welcome,name = 'welcome'),
+    url(r'^$', views.personal_of_day, name = 'personalToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_personal,name = 'pastPersonal'), 
 ]
